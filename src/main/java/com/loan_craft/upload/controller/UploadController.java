7f9dd42d14baeaf6031c3f6a80e4b9292390a517
Nipur Gupta <nipurgupta047@gmail.com>
2025-10-15 03:17:44 +0530

    added health api

--- src/main/java/com/loan_craft/upload/controller/UploadController.java
+++ src/main/java/com/loan_craft/upload/controller/UploadController.java
@@ -50,4 +50,9 @@ public class UploadController {
                 .body(new ApiGenericResponse<>(true, "Application submitted", null, res));
     }
 
+    @GetMapping(value = "health")
+    public String healthApi(){
+        return "Working";
+    }
+
 }
